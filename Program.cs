4eb60e86dc72c91b373c671cd73ed06abc494109
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 11:14:22 +0700

    menambahkan class FanLaptop

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿internal class Program
+﻿using System.Xml.Linq;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         KodeProduk kodeProduk = new KodeProduk();
         string produk = Console.ReadLine();     
         Console.WriteLine(kodeProduk.getKodeProduk(produk));
+
+        FanLaptop fan = new FanLaptop();
+        fan.TurboShortcut();
+        fan.ModeDown();
+        fan.ModeDown();
+        fan.ModeDown();
     }
 }
 
@@ -39,5 +47,121 @@ public class KodeProduk
         {
             return "Kode pos tidak ditemukan";
         }
+    }
+}
+
+public interface IModeState
+{
+    void ModeUp(FanLaptop fan);
+    void ModeDown(FanLaptop fan);
+    void DisplayState();
+}
+
+public class QuiteMode : IModeState
+{
+    public void ModeUp(FanLaptop fan)
+    {
+        fan.SetState(new BalanceMode());
+        
+    }
+    public void ModeDown(FanLaptop fan)
+    {
+        Console.WriteLine("Fan Quiet (Sudah Mode Terendah)");
+    }
+    public void DisplayState()
+    {
+        Console.WriteLine("Fan Quiet");
+    }
+}
+
+public class BalanceMode : IModeState
+{
+    public void ModeUp(FanLaptop fan)
+    {
+        fan.SetState(new PerformanceMode());
+    }
+    public void ModeDown(FanLaptop fan)
+    {
+        fan.SetState(new QuiteMode());
+    }
+    public void DisplayState()
+    {
+        Console.WriteLine("Fan Balance");
+    }
+}
+
+public class PerformanceMode : IModeState
+{
+    public void ModeUp(FanLaptop fan)
+    {
+        fan.SetState(new TurboMode());
+    }
+    public void ModeDown(FanLaptop fan)
+    {
+        fan.SetState(new BalanceMode());
+    }
+    public void DisplayState()
+    {
+        Console.WriteLine("Fan Performa");
+    }
+}
+
+public class TurboMode : IModeState
+{
+    public void ModeUp(FanLaptop fan)
+    {
+        Console.WriteLine("Fan Turbo (Sudah Mode Tertinggi)");
+    }
+    public void ModeDown(FanLaptop fan)
+    {
+        fan.SetState(new PerformanceMode());
+    }
+    public void DisplayState()
+    {
+        Console.WriteLine("Fan Turbo");
+    }
+}
+
+public class FanLaptop
+{
+    private IModeState _currentState;
+
+    public FanLaptop()
+    {
+        _currentState = new QuiteMode();
+        _currentState.DisplayState();
+    }
+
+    public void SetState(IModeState newState)
+    {
+        _currentState = newState;
+        _currentState.DisplayState();
+    }
+
+    public void ModeUp()
+    {
+        Console.Write(" Berubah Menjadi ");
+        _currentState.ModeUp(this);
+
+    }
+
+    public void ModeDown()
+    {
+        Console.WriteLine(" Berubah Menjadi ");
+        _currentState.ModeDown(this);
+    }
+
+    public void TurboShortcut()
+    {
+        if (_currentState is QuiteMode)
+        {
+            Console.WriteLine(" Berubah Menjadi ");
+            SetState(new TurboMode());
+        }
+        else if (_currentState is TurboMode) {
+            Console.WriteLine(" Berubah Menjadi ");
+            SetState(new QuiteMode());
+        }
+
     }
 }
